9ef283eb15ed6dda16221c5cfd8758a85491ab7e
Donybro <dony1bro@gmail.com>
2022-08-29 23:32:43 +0500

    main page initial logic

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="bg-red-400">
-    hello
+  <div class="section_wrapper">
+    <search-bar class="mt-[30px]" @search="searchBooks"/>
+    <book-table v-if="!loading && !error" :table-body="booksList"/>
+    <loading v-if="loading"/>
+    <p v-if="error" class="error_message">{{error}}</p>
   </div>
 </template>
 
 <script>
+import SearchBar from '~/components/SearchBar'
+
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: { SearchBar },
+  data () {
+    return {
+      loading: true,
+      error:''
+    }
+  },
+  methods: {
+    async searchBooks (searchText) {
+      try {
+        this.loading = true
+        await this.$store.dispatch('books/fetchBooks',searchText)
+      } catch (e) {
+        this.error = e
+      } finally {
+        this.loading = false
+      }
+    }
+  },
+  computed:{
+    booksList(){
+      return this.$store.getters['books/getFilteredBooksList']
+    }
+  }
 }
 </script>
