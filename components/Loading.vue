b457932f7f0f34cf863e4397a89e00b7ae8a840b
Donybro <dony1bro@gmail.com>
2022-08-29 23:24:34 +0500

    added loading component

---
+++ components/Loading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="font-montserrat text-primary text-center">
+    ...Loading
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
+
+<style scoped>
+
+</style>
