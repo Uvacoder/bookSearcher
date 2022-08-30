8ae6b4f67a137bb9897199025105432668ab6a77
Donybro <dony1bro@gmail.com>
2022-08-30 18:01:45 +0500

    auth page done

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="h-screen w-full bg-gray-50 flex items-center justify-center">
-      <auth-form class="w-[50%]"/>
+      <auth-form class="w-[50%]" @submit="login"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'auth'
+  name: 'auth',
+  methods: {
+    async login (username) {
+      const isValidUserName = await this.$store.dispatch('auth/login',username)
+      if(isValidUserName){
+        await this.$router.push('/')
+        alert('Welcome!')
+      }else{
+        alert(`Not valid username :${username}`)
+      }
+    }
+  },
 }
 </script>
 
