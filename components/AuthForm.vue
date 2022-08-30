78e1b880e1c5892d464873a29b2db04f83e3e0da
Donybro <dony1bro@gmail.com>
2022-08-30 17:11:08 +0500

    auth validation done with some bugs

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -1,62 +1,71 @@
 <template>
   <div class="rounded-[8px] font-montserrat bg-white shadow px-[20px] py-[15px]">
-    <div class="flex flex-col gap-[10px]">
+    <form class="flex flex-col gap-[10px]">
       <label class="text-[2em]" for="username">Username</label>
       <input maxlength="16" class="bg-gray-50 shadow focus:outline-none focus:ring-2 focus:ring-primary placeholder:text-[1.12em] text-[1.12em] text-primary py-[12px] px-[20px]" id="username" v-model="username" type="text">
       <div class="error_message">{{error}}</div>
       <button :disabled="error" @click.prevent="submitHandler" class="font-montserrat bg-primary block text-white hover:opacity-50 h-full px-[20px] py-[10px]" :class="{'opacity-40 cursor-not-allowed':error}">
         {{ loading ? "Loading" : "GO"  }}
       </button>
-    </div>
+    </form>
   </div>
 </template>
 
 <script>
+import * as Promise from "bluebird";
 export default {
   name: 'AuthForm',
   data () {
     return {
       username: '',
       error:'',
-      loading:false
+      loading:false,
+      validationController:new AbortController()
     }
   },
   watch:{
-    async username(token){
-      await this.validateToken(token)
-      this.loading = false
+    async username(val){
+      if(this.loading){
+        this.validationController.abort()
+      }
+      await this.validationHandler(val.split(' ').join(''))
     },
     loading(val){
       console.log(val)
     }
   },
   methods: {
+    async validationHandler(token){
+      try{
+        this.loading = true
+        const validationDone =  await this.validateToken(token)
+        if(validationDone){
+          this.error = ""
+        }
+      }catch (e) {
+        this.error =  e
+      }finally {
+        this.loading = false
+      }
+    },
     submitHandler () {
-      this.$emit('submit',this.login)
+      this.$emit('submit',this.username)
     },
-    checkTokenForLength(token){
-      const valid = token.length === 16
-      if(!valid){
-        this.error = "*The USERNAME's length should be 16-characters"
-      }else{
-        this.error = ''
-      }
-    },
-    checkTokenForLatinCharacters(token){
-      const valid =  /^[a-zA-Z\s.,]+$/.test(token)
-      if(!valid && this.username !== ''){
-        this.error = '*The USERNAME should not has any special characters, nor numbers or cyrillic symbols,latin symbols only!'
-      }else{
-        this.error = ''
-      }
-    },
-    validateToken (token) {
-     return new Promise((res,rej)=>{
-       this.loading = true
-       setTimeout(()=>{
-         this.checkTokenForLatinCharacters(token)
-         this.checkTokenForLength(token)
-         res()
+    validateToken(token) {
+      return new Promise((res,rej)=>{
+        this.validationController.signal.addEventListener("abort", () => {
+          res(false)
+        });
+        setTimeout(async ()=>{
+          let lengthValid = token.length === 16
+          let wordsValid =  /^[a-zA-Z\s.,]+$/.test(token)
+          if(!lengthValid){
+            rej("*The USERNAME's length should be 16-characters")
+          }
+          else if(!wordsValid && this.username !== ''){
+            rej('*The USERNAME should not has any special characters, nor numbers or cyrillic symbols,latin symbols only!')
+          }
+          else res(true)
        },3000)
      })
     }
